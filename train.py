6973f8bda4d37e57e5d13a458954bda77ad50647
Alexandra <a_chubcheva@a.chubcheva>
2020-05-09 23:58:00 +0300

    Add p-values

--- train.py
+++ train.py
@@ -3,10 +3,13 @@ import torch.nn as nn
 import torch.optim as optim
 from tqdm import trange, tqdm_notebook
 import utils.pytorch_utils as ptu
+from utils.exp1 import experiment_gan_plot, experiment_data
 import numpy as np
+from IPython.display import clear_output
+import scipy.stats as stat
 
 def train(generator, critic, c_loss_fn, g_loss_fn, 
-          train_loader, g_optimizer, c_optimizer, 
+          train_loader, g_optimizer, c_optimizer, gp_lamb=0.001,
           n_critic=1, g_scheduler=None, c_scheduler=None, 
           weight_clipping=None):
     g_losses, c_losses = [], []
@@ -14,14 +17,13 @@ def train(generator, critic, c_loss_fn, g_loss_fn,
     critic.train()
     for i, x in enumerate(train_loader):
         x = x.to(ptu.device).float()
+        fake_data = generator.sample(x.shape[0])
+
         c_loss = c_loss_fn(generator, critic, x)
         c_optimizer.zero_grad()
         c_loss.backward()
         c_optimizer.step()
         c_losses.append(c_loss.item())
-        if weight_clipping is not None:
-            for param in critic.parameters():
-                param.data.clamp_(-weight_clipping, weight_clipping)
 
         if i % n_critic == 0:
             g_loss = g_loss_fn(generator, critic, x)
@@ -37,35 +39,46 @@ def train(generator, critic, c_loss_fn, g_loss_fn,
 
 def train_epochs(generator, critic, g_loss_fn, c_loss_fn, 
                  train_loader, train_args, g_opt, c_opt, 
-                 g_scheduler=None, c_scheduler=None):
+                 g_scheduler=None, c_scheduler=None, is_spiral=False):
     epochs = train_args['epochs']
 
     train_losses = dict()
+    data = experiment_data(is_spiral=is_spiral)
+    pvals = []
     for epoch in tqdm_notebook(range(epochs), desc='Epoch', leave=False):
         if epoch == 1:
             start_snapshot = get_training_snapshot(generator, critic)
         generator.train()
         critic.train()
         train_loss = train(generator, critic, c_loss_fn, g_loss_fn, train_loader, 
-                           g_opt, c_opt, n_critic=train_args.get('n_critic', 1), 
-                           g_scheduler=g_scheduler, c_scheduler=c_scheduler,
-                           weight_clipping=train_args.get('weight_clipping', None))
+                           g_opt, c_opt, gp_lamb=train_args.get('n_critic', 0.001),
+                           n_critic=train_args.get('n_critic', 1), 
+                           g_scheduler=g_scheduler, c_scheduler=c_scheduler)
         
         for k in train_loss:
             if k not in train_losses:
                 train_losses[k] = []
             train_losses[k].extend(train_loss[k])
-
+        sample = get_training_snapshot(generator, critic)
+        
+        # stat criterion
+        data2 = np.array(sample)
+        data2 = data2.T
+        data1 = data.T
+        pvalue = stat.ks_2samp(data1[0], data2[0])[1]
+        pvals.append(pvalue)
+        
+        experiment_gan_plot(data, sample, f'Epoch {epoch}',
+                            f'results/epoch_{epoch}.png', is_spiral)
+        clear_output(wait=True)
     if train_args.get('final_snapshot', False):
         final_snapshot = get_training_snapshot(generator, critic)
-        return [train_losses, *start_snapshot, *final_snapshot]
+        return (train_losses, start_snapshot, final_snapshot, pvals)
     else:
-        return train_losses
+        return train_losses, pvals
 
 def get_training_snapshot(generator, critic, n_samples=5000):
     generator.eval()
     critic.eval()
-    xs = np.linspace(-1, 1, 1000)
     samples = ptu.get_numpy(generator.sample(n_samples))
-    critic_output = ptu.get_numpy(critic(ptu.FloatTensor(xs).unsqueeze(1)))
-    return samples, xs, critic_output
+    return samples
--- utils/exp1.py
+++ utils/exp1.py
@@ -28,37 +28,53 @@ def plot_gan_training(losses, title, fname):
     plt.ylabel('Loss')
     savefig(fname)
 
-def experiment_gan_plot(data, samples, xs, ys, title, fname):
-    plt.figure()
-    plt.hist(samples, bins=50, density=True, alpha=0.7, label='fake')
-    plt.hist(data, bins=50, density=True, alpha=0.7, label='real')
-
-    plt.plot(xs, ys, label='discrim')
+def experiment_gan_plot(data, samples, title, fname, is_spiral=False):
+    plt.figure(figsize=(8,8))
+    if is_spiral:
+        plt.scatter(data[:, 0], data[:, 1], label='real')
+        plt.scatter(samples[:, 0], samples[:, 1], label='fake')
+    else:
+        plt.hist(samples, bins=50, density=True, alpha=0.7, label='fake')
+        plt.hist(data, bins=50, density=True, alpha=0.7, label='real')
     plt.legend()
+    plt.grid()
     plt.title(title)
     savefig(fname)
 
-def experiment_data(n=20000):
-    assert n % 2 == 0
-    gaussian1 = np.random.normal(loc=-1, scale=0.25, size=(n//4,))
-    gaussian2 = np.random.normal(loc=0.5, scale=0.55, size=(n//2,))
-    gaussian3 = np.random.normal(loc=8, scale=0.25, size=(n//4,))
-    gaussian4 = np.random.normal(loc=5, scale=0.55, size=(n//8,))
+def experiment_data(n=20000, is_spiral=False):
+    if is_spiral:
+        theta = np.random.rand(n) * 30 
+        r = theta / 30
+        results =np.zeros((n, 2))
+        results[:, 0] = r * (np.cos(theta) + np.random.rand(n) / 7)
+        results[:, 1] = r * (np.sin(theta) + np.random.rand(n) / 7)
+        return results
+    else:
+        assert n % 2 == 0
+        gaussian1 = np.random.normal(loc=-1, scale=0.25, size=(n//2,))
+        gaussian2 = np.random.normal(loc=0.5, scale=0.55, size=(n//2,))
+    #    gaussian3 = np.random.normal(loc=8, scale=0.25, size=(n//4,))
+    #    gaussian4 = np.random.normal(loc=5, scale=0.55, size=(n//8,))
+#         data = (np.concatenate([gaussian1])).reshape([-1, 1])
+        data = (np.concatenate([gaussian1, gaussian2]) + 1).reshape([-1, 1])
+        scaled_data = (data - np.min(data)) / (np.max(data) - np.min(data) + 1e-8)
+        return 2 * scaled_data -1
 
-    data = (np.concatenate([gaussian1, gaussian2, gaussian3, gaussian4]) + 1).reshape([-1, 1])
-    scaled_data = (data - np.min(data)) / (np.max(data) - np.min(data) + 1e-8)
-    return 2 * scaled_data -1
-
-def visualize_experiment_dataset():
-    data = experiment_data()
-    plt.hist(data, bins=50, alpha=0.7, label='train data')
+def visualize_experiment_dataset(is_spiral=False):
+    data = experiment_data(is_spiral=is_spiral)
+    plt.figure(figsize=(8,8))
+    if is_spiral:
+        plt.scatter(data[:, 0], data[:, 1], label='train spiral data')
+    else:
+        plt.hist(data, bins=50, alpha=0.7, label='train data')
     plt.legend()
     plt.show()
 
-
-def experiment_save_results(part, fn, name):
-    data = experiment_data()
-    losses, samples1, xs1, ys1, samples_end, xs_end, ys_end = fn(data)
+def experiment_save_results(part, fn, name, is_spiral=False):
+    data = experiment_data(is_spiral=is_spiral)
+    g, c, losses, samples_start, samples_end, pvals = fn(data)
     plot_gan_training(losses, f'{name}{part} Losses', f'results/{name}{part}_losses.png')
-    experiment_gan_plot(data, samples1, xs1, ys1, f'{part}{part} Epoch 1', f'results/{name}{part}_epoch1.png')
-    experiment_gan_plot(data, samples_end, xs_end, ys_end, f'{name}{part} Final', f'results/{name}{part}_final.png')
+    experiment_gan_plot(data,  samples_start, f'{name}{part} Epoch 1', f'results/{name}{part}_epoch1.png', is_spiral)
+    experiment_gan_plot(data, samples_end, f'{name}{part} Final', f'results/{name}{part}_final.png', is_spiral)
+    return g, c, losses, samples_start, samples_end, pvals
+
