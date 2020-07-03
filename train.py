28f573c78b1033f93f65fece81050b10d6cf96aa
maximzubkov <zubkov.md@phystech.edu>
2020-07-04 01:46:43 +0300

    Refactoring experiments

--- train.py
+++ train.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.optim as optim
 from tqdm import trange, tqdm_notebook
 import utils.pytorch_utils as ptu
-from utils.exp1 import experiment_gan_plot, experiment_data, plot_dicriminator_heatmap, show_qq_plot
 import numpy as np
 from IPython.display import clear_output
 import matplotlib.pyplot as plt
@@ -16,7 +15,7 @@ def train(generator, critic, c_loss_fn, g_loss_fn,
           train_loader, g_optimizer, c_optimizer, gp_lamb=0.001,
           n_critic=1, g_scheduler=None, c_scheduler=None, 
           weight_clipping=None):
-    g_losses, g_grad, c_grad, c_losses, accuracy = [], [], [], [], []
+    g_losses, g_grad, c_grad, c_losses = [], [], [], []
     generator.train(True)
     critic.train(True)
     for i, x in enumerate(train_loader):
@@ -51,18 +50,20 @@ def train(generator, critic, c_loss_fn, g_loss_fn,
                 g_scheduler.step()
             if c_scheduler is not None:
                 c_scheduler.step()
-    return dict(g_losses=g_losses, c_losses=c_losses, accuracy=accuracy, g_grad=g_grad, c_grad=c_grad)
+    return dict(g_losses=g_losses, 
+                c_losses=c_losses, 
+                g_grad=g_grad, 
+                c_grad=c_grad)
 
-def train_epochs(generator, critic, g_loss_fn, c_loss_fn,
+def train_epochs(experiment, generator, critic, g_loss_fn, c_loss_fn,
                  train_loader, train_args, g_opt, c_opt, 
-                 g_scheduler=None, c_scheduler=None, is_spiral=False, modes=1, param_modes=[(0,1)], name=""):
+                 g_scheduler=None, c_scheduler=None,  name=""):
     epochs = train_args['epochs']
 
-    train_losses = dict()
-    train_losses["pvals"] = []
-    train_losses["accuracy"] = []
-    train_losses["hist_diff"] = []
-    data = experiment_data(is_spiral=is_spiral, n_modes=modes, params=param_modes, n=8192)
+    train_logs = dict()
+    train_logs["pvals"] = []
+    train_logs["accuracy"] = []
+    data = experiment.dataset(n=8192)
 
     for epoch in tqdm_notebook(range(epochs), desc='Epoch', leave=False):
         if epoch == 0:
@@ -75,86 +76,37 @@ def train_epochs(generator, critic, g_loss_fn, c_loss_fn,
                            g_scheduler=g_scheduler, c_scheduler=c_scheduler)
         
         for k in train_loss:
-            if k not in train_losses:
-                train_losses[k] = []
-            train_losses[k].extend(train_loss[k])
+            if k not in train_logs:
+                train_logs[k] = []
+            train_logs[k].extend(train_loss[k])
         sample = get_training_snapshot(generator, critic)
 
 
-        if not is_spiral:
-            # stat criterion
-            with torch.no_grad():
-                generator.train(False)
-                critic.train(False)
-                data1 = torch.Tensor(experiment_data(n=250, is_spiral=is_spiral, n_modes=modes, params=param_modes))
-                data2 = generator.sample(250)
-                preds1 = critic(data2).detach().numpy()
-                preds2 = critic(data1).detach().numpy()
-                predictions_false = preds1 <= 0.5
-                predictions_true = preds2 > 0.5
-                correct = np.sum(predictions_false) + np.sum(predictions_true)
-                acc = correct / (data1.shape[0] * 2)
-                train_losses["accuracy"].append(acc)
-                generator.train(True)
-                critic.train(True)
-                data1 = data1.detach().numpy()
-                data2 = data2.detach().numpy()
-                pvalue = stat.ks_2samp(data1.T[0], data2.T[0])[1]
-                train_losses["pvals"].append(pvalue)
+        evaluation_results = experiment.eval(generator, critic)
+        for k in evaluation_results:
+            if k not in train_logs:
+                train_logs[k] = []
+            try:
+                train_logs[k].extend(evaluation_results[k])
+            except TypeError:
+                train_logs[k].append(evaluation_results[k])
 
-        # clear_output(wait=True)
-        if is_spiral:
-            fig = plt.figure(figsize=(20,10))
-            plt.rc("text", usetex=True)
-
-            ax1 = fig.add_subplot(1, 2, 1)
-            ax2 = fig.add_subplot(1, 2, 2)
-            
-            experiment_gan_plot(data, sample, f'Epoch {epoch}', ax=ax1, is_spiral=True)
-            plot_dicriminator_heatmap(critic, fig=fig, ax=ax2)
-            plt.show()
-        else:
-            # fig = plt.figure(figsize=(20,20))
-            # plt.rc("text", usetex=True)
-            # ax = fig.add_subplot(1, 1, 1)
-            # experiment_gan_plot(data, data2, f'Epoch {epoch}', ax=ax, is_spiral=False)
-            # plt.savefig(f"results/{name}/hist_{epoch}.pdf") 
-            # plt.show()
-            if epoch == epochs - 1:
-                fig = plt.figure(figsize=(20,20))
-                plt.rc("text", usetex=True)
-                ax1 = fig.add_subplot(2, 1, 1)
-                ax2 = fig.add_subplot(2, 1, 2)
-                ax1.plot(train_losses["g_grad"])
-                ax1.set_title("Norm of Generator gradient", fontsize=45)
-                ax1.set_xlabel('Training Iteration',fontsize=42)
-                ax1.set_ylabel('Norm of gradient',fontsize=42)
-                ax1.tick_params(axis="x", labelsize=40)
-                ax1.tick_params(axis="y", labelsize=40)
-                ax1.grid()
-                
-                ax2.plot(train_losses["c_grad"])
-                ax2.set_title("Norm of Discriminator gradient", fontsize=45)
-                ax2.set_xlabel('Training Iteration',fontsize=42)
-                ax2.set_ylabel('Norm of gradient',fontsize=42)
-                ax2.tick_params(axis="x", labelsize=40)
-                ax2.tick_params(axis="y", labelsize=40)
-                ax2.grid()
-
-                plt.savefig(f"results/{name}/output_{epoch}.pdf") 
-            plt.show()
+        if epoch % 2 == 0:
+            clear_output(wait=True)
+            experiment.epoch_vizual(train_logs, path=f"results/{name}/output_{epoch}_tmp.pdf")
         
     if train_args.get('final_snapshot', False):
         final_snapshot = get_training_snapshot(generator, critic)
-        return (train_losses, start_snapshot, final_snapshot)
+        return (train_logs, start_snapshot, final_snapshot)
     else:
-        return train_losses
+        return train_logs
 
 def get_training_snapshot(generator, critic, n_samples=10000):
-    generator.train(False)
-    critic.train(False)
     with torch.no_grad():
-        samples = ptu.get_numpy(generator.sample(n_samples))
-    generator.train(True)
-    critic.train(True)
-    return samples
+        generator.train(False)
+        critic.train(False)
+        with torch.no_grad():
+            samples = ptu.get_numpy(generator.sample(n_samples))
+        generator.train(True)
+        critic.train(True)
+        return samples
--- utils/utils.py
+++ utils/utils.py
@@ -26,3 +26,74 @@ def save_training_plot(train_losses, test_losses, title, fname):
     plt.xlabel('Epoch')
     plt.ylabel('NLL')
     savefig(fname)
+
+def visualize_train_epoch():
+    if is_spiral:
+        spiral_epoch_vizual()
+    else:
+        hist_epoch_vizual()
+
+def compare_pvals(path="results/compare_sn_tmp.pdf"):
+    plt.figure(figsize=(20, 20))
+    ax1=plt.subplot(2, 1, 1)
+    ax2=plt.subplot(2, 1, 2)
+    ax1.set_title("SNGAN p-value", fontsize=45)
+    ax2.set_title("SNGAN Generator grad norm", fontsize=45)
+    for n_cr in [2, 4, 6]:
+        ax1.semilogy(np.load(f"results/sn_ncr{n_cr}/p.npy"), label="$n_{cr}= $"+f" {n_cr}")
+        ax1.set_xlabel('Training Iteration',fontsize=42)
+        ax1.set_ylabel('p-value',fontsize=42)
+        ax1.tick_params(axis="x", labelsize=40)
+        ax1.tick_params(axis="y", labelsize=40)
+        ax2.semilogy(np.load(f"results/sn_ncr{n_cr}/g_grad.npy"), label="$n_{cr}= $"+f" {n_cr}")
+        ax2.set_xlabel('Training Iteration',fontsize=42)
+        ax2.set_ylabel('Generator Grad Norm',fontsize=42)
+        ax2.tick_params(axis="x", labelsize=40)
+        ax2.tick_params(axis="y", labelsize=40)
+    ax1.grid()
+    ax1.legend(fontsize=30)
+    ax2.grid()
+    ax2.legend(fontsize=30)
+        
+    savefig(path)
+
+def experiment_save_results(experiment):
+    g, c, losses, samples_start, samples_end = experiment.run()
+    experiment.final_plot(g, c, losses, samples_start, samples_end)
+    return g, c, losses
+
+def sn2nosn(path="results/compare_tmp.pdf"):
+    plt.figure(figsize=(30, 40))
+    ax = {}
+    ax[2] = {}
+    ax[4] = {}
+    ax[6] = {}
+    ax[2][0]=plt.subplot(3, 2, 1)
+    ax[2][1]=plt.subplot(3, 2, 2)
+    ax[4][0]=plt.subplot(3, 2, 3)
+    ax[4][1]=plt.subplot(3, 2, 4)
+    ax[6][0]=plt.subplot(3, 2, 5)
+    ax[6][1]=plt.subplot(3, 2, 6)
+    for n_cr in [2, 4, 6]:
+        ax[n_cr][0].set_title("$n_{cr}= $"+f" {n_cr}", fontsize=45)
+        ax[n_cr][0].semilogy(np.load(f"results/sn_ncr{n_cr}/p.npy"), label=f"SNGAN")
+        ax[n_cr][0].semilogy(np.load(f"results/ncr{n_cr}/p.npy"), label=f"noSNGAN")
+        ax[n_cr][0].set_xlabel('Training Iteration',fontsize=42)
+        ax[n_cr][0].set_ylabel('p-value',fontsize=42)
+        ax[n_cr][0].tick_params(axis="x", labelsize=40)
+        ax[n_cr][0].tick_params(axis="y", labelsize=40)
+        ax[n_cr][0].grid()
+        ax[n_cr][0].legend(fontsize=30)
+        x = [300 * x for x in range(len(np.load(f"results/sn_ncr{n_cr}/g_grad.npy")))]
+        ax[n_cr][1].set_title("$n_{cr}= $"+f" {n_cr}", fontsize=45)
+        ax[n_cr][1].semilogy(x, np.load(f"results/sn_ncr{n_cr}/g_grad.npy"), label=f"SNGAN")
+        x = [300 * x  for x in range(len(np.load(f"results/ncr{n_cr}/g_grad.npy")))]
+        ax[n_cr][1].semilogy(x, np.load(f"results/ncr{n_cr}/g_grad.npy"), label=f"noSNGAN")
+        ax[n_cr][1].set_xlabel('Training Iteration',fontsize=42)
+        ax[n_cr][1].set_ylabel('Generator Grad Norm',fontsize=42)
+        ax[n_cr][1].tick_params(axis="x", labelsize=40)
+        ax[n_cr][1].tick_params(axis="y", labelsize=40)
+        ax[n_cr][1].grid()
+        ax[n_cr][1].legend(fontsize=30)
+        
+    savefig(path)
