7a6242bfd92d9da14bcfd5e10cf91e6801ea0537
maximzubkov <zubkov.md@phystech.edu>
2020-08-01 20:22:31 +0300

    Refactor configs

--- configs/__init__.py
+++ configs/__init__.py
@@ -1,21 +1,27 @@
 from src import generator_w_loss, critic_w_loss
-from .gan_config import GANConfig, MLP_params
+from .gan_config import GANConfig, MLPConfig
 
 
-def get_gan_default_config(n_cr: int, exp_name: str, sn: bool) -> GANConfig:
+def get_gan_default_config(n_cr: int, exp_name: str, spec_norm: bool) -> GANConfig:
+    c_config = MLPConfig(input_size=1, n_hidden=3, hidden_size=25, output_size=1, spec_norm=spec_norm)
+    g_config = MLPConfig(input_size=3, n_hidden=3, hidden_size=25, output_size=1, spec_norm=spec_norm)
     return GANConfig(
         batch_size=64,
         n_epochs=50,
         n_cr=n_cr,
+
         c_lr=9e-5,
         c_betas=(0, 0.9),
-        c_params=(1, 3, 25, 1),
+
+        c_config=c_config,
+        g_config=g_config,
+
         c_loss=critic_w_loss,
+        g_loss=generator_w_loss,
+
         g_lr=9e-5,
         g_betas=(0, 0.9),
-        g_params=(3, 3, 25, 1),
-        g_loss=generator_w_loss,
+
         exp_name=exp_name,
-        sn=sn,
         dataset_params=[(2, 0.4), (0, 0.55), (5, 0.25)],
     )
--- configs/gan_config.py
+++ configs/gan_config.py
@@ -1,7 +1,14 @@
 from dataclasses import dataclass
 from typing import Tuple, Callable, List
 
-MLP_params = Tuple[int, int, int, int]
+
+@dataclass(frozen=True)
+class MLPConfig:
+    input_size: int
+    n_hidden: int
+    hidden_size: int
+    output_size: int
+    spec_norm: bool
 
 
 @dataclass(frozen=True)
@@ -9,14 +16,18 @@ class GANConfig:
     batch_size: int
     n_epochs: int
     n_cr: int
+
     c_lr: float
     c_betas: Tuple[float, float]
-    c_params: MLP_params
+
+    c_config: MLPConfig
+    g_config: MLPConfig
+
     c_loss: Callable
+    g_loss: Callable
+
     g_lr: float
     g_betas: Tuple[float, float]
-    g_params: MLP_params
-    g_loss: Callable
+
     exp_name: str
-    sn: bool
     dataset_params: List[Tuple[float, float]]
